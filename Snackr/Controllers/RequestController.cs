a13ba37c505f0c67bcb772f7eea13d399f12cb51
David Ko <davidk@genebygene.com>
2018-01-12 10:57:13 -0600

    Added proper log in for gene by gene domain. Added restyling for profile page. Added request redirect based on authenticated or not, added access denied page.

--- Snackr/Controllers/RequestController.cs
+++ Snackr/Controllers/RequestController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Snackr.Controllers
 {
     public class RequestController : Controller
     {
         // GET
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
